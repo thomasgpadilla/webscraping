901d997a16c2b74bf903265d595e9e96e20cd334
Thomas Padilla <tgpadillajr@gmail.com>
2016-08-03 16:02:26 -0700

    Update and rename frus_pdf_urls.py to frus_singlevolume_pdf_urls.py

--- frus_singlevolume_pdf_urls.py
+++ frus_singlevolume_pdf_urls.py
@@ -2,13 +2,13 @@ from urllib.request import urlopen
 from bs4 import BeautifulSoup
 import requests
 
-#point to FRUS volume page
+#point to single FRUS volume page
 html = urlopen("http://digicoll.library.wisc.edu/cgi-bin/FRUS/FRUS-idx?type=header&id=FRUS.FRUS1932v05")
 soup = BeautifulSoup(html, 'html.parser')
 
 with open("fdr_pdf_urls.txt", "w") as file:
 
-#scrape FRUS volume section relative URLs 
+#scrape FRUS volume relative URLs 
 	for links in soup.find_all("p", {"class":"cntsitem"}):
 
 #prepend relative URLs with base URL
